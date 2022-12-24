e068a636519dff61877c827596ea1cb4a2dd1feb
@Mariy32 <zyabliceva.mariy@bk.ru>
2022-12-24 10:22:26 +0300

    ДPпятница

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+// Console.Write("Введите числа, разделяя запятой");
+// int[] numbers = StringToNum(Console.ReadLine());
+// PrintArray(numbers);
+// int sum = 0;
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     if (numbers[i] > 0)
+//     {
+//         sum++;
+//     }
+// }
+// Console.WriteLine();
+// Console.WriteLine($"Количество чисел, которые > 0    {sum}");
+
+
+// int[] StringToNum(string input)
+// {
+//     int count = 1;
+//     for (int i = 0; i < input.Length; i++)
+//     {
+//         if (input[i] == ',')
+//         {
+//             count++;
+//         }
+//     }
+
+//     int[] numbers = new int [count];
+//     int index = 0;
+
+//     for (int i = 0; i < input.Length; i++)
+//     {
+//         string temp = "";
+
+//         while (input [i] != ',')
+//         {
+//         if(i != input.Length - 1)
+//         {
+//             temp += input [i].ToString();
+//             i++;
+//         }
+//         else
+//         {
+//             temp += input [i].ToString();
+//             break;
+//         }
+//         }
+//         numbers[index] = Convert.ToInt32(temp);
+//         index++;
+//     }
+//     return numbers;
+// }
+
+
+// void PrintArray(int[] array)
+// {
+    
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+    
+// }
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите b1:");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k1:");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b2:");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k2:");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+
+Console.WriteLine($"точка пересечения прямых имеет координаты X: {x}, Y: {y}");
